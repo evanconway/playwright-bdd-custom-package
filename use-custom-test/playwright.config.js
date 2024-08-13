6f8da87962e2d61f64959de8c2fd22dff1dd3b8a
Evan Conway <43253820+evanconway@users.noreply.github.com>
2024-08-12 23:35:35 -0400

    get custom-test working

--- use-custom-test/playwright.config.js
+++ use-custom-test/playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig, defineBddConfig } from 'custom-test';
 
 const testDir = defineBddConfig({
   features: 'sample.feature',
-  steps: ['steps.ts', 'customTest.ts'],
+  steps: ['steps.ts', './node_modules/custom-test/dist/index.js'],
 });
 
 export default defineConfig({
